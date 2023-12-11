6c787fac91c64a4df6e60ba9eae1126e41f86238
Chiauwen <chiauwen28@gmail.com>
2023-12-11 18:46:13 +0800

    updated folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.js'
+import './index.css'
+import {  
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+} from './pages'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: < temp/>,
+  },
+
+  {
+    path: '/',
+    element: < temp/>,
+  },
+
+  {
+    path: '/',
+    element: < temp/>,
+  },
+
+  {
+    path: '/',
+    element: < temp/>,
+  },
+
+  {
+    path: '/',
+    element: < temp/>,
+  },
+
+])
+
+ReactDOM.render(
+  <RouterProvider router={router} />,
+  document.getElementById('root')
+)
