e198cb859582d71861b7fec59c1f4f3530392db8
JEAN\szuje <szujean45@gmail.com>
2023-12-15 09:21:59 +0800

    Temporarily update detectitem

--- frontend/src/pages/DetectItem.jsx
+++ frontend/src/pages/DetectItem.jsx
@@ -3,9 +3,48 @@ import Webcam from 'react-webcam';
 import axios from 'axios';
 
 const DetectItem = () => {
+  const [generatedImage, set_generatedImage] = useState(null)
   const [image, setImage] = useState(null);
   const [showCamera, setShowCamera] = useState(false);
   const [detectionResult, setDetectionResult] = useState(null);
+
+  const imageGenerator = async (trashType) => {
+    if (trashType === "") {
+      return 0;
+    }
+
+    try {
+      const response = await fetch(
+        "https://api.openai.com/v1/images/generations",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer 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`,
+          },
+          body: JSON.stringify({
+            prompt: `Beautiful, exciting, and craftable handicraft projects using the trash type ${trashType}.`,
+            size: "1024x1024",
+            n: 1,
+          }),
+        }
+      );
+
+      const data = await response.json();
+      console.log(data);
+
+      set_generatedImage(data.generatedImage);
+    } catch (error) {
+      console.error('Error generating image:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (detectionResult && detectionResult.length > 0) {
+      const trashType = detectionResult[0].trash_type;
+      imageGenerator(trashType);
+    }
+  }, [detectionResult]);
 
   const handleImageUpload = async (e) => {
     const file = e.target.files[0];
@@ -88,6 +127,13 @@ const DetectItem = () => {
               </p>
             </div>
           ))}
+        </div>
+      )}
+
+      {generatedImage && (
+        <div>
+          <h2>Generated Ideas</h2>
+          <img src={generatedImage} alt="Craft Ideas" style={{ maxWidth: '100%' }} />
         </div>
       )}
     </div>
